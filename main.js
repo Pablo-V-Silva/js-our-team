ab4408b62086638b457fb10a48495ee20552ab70
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-10-22 15:01:24 +0200

    console.log of my team

---
+++ main.js
@@ -0,0 +1,31 @@
+const team = [
+  {
+    name: 'Garen',
+    role: 'Top liner',
+    img: 'https://picsum.photos/id/1025/300/300'
+  },
+  {
+    name: 'Jax',
+    role: 'mid liner',
+    img: 'https://picsum.photos/id/1025/300/300'
+  },
+  {
+    name: 'Jhin',
+    role: 'first bottom liner',
+    img: 'https://picsum.photos/id/1025/300/300'
+  },
+  {
+    name: 'Leona',
+    role: 'second bottom liner',
+    img: 'https://picsum.photos/id/1025/300/300'
+  },
+  {
+    name: 'Amumu',
+    role: 'Jungler',
+    img: 'https://picsum.photos/id/1025/300/300'
+  }
+]
+
+for (let key in team) {
+  console.log(team[key]);
+}
